75367d486b172f6059c4d4021f31d4b132dd0204
Raizo <raizo.cn@gmail.com>
2018-11-23 21:22:36 +0800

    Implemented Home, Details and Location pages. (The register function is not finished)

--- EventManagementSystemApp/www/js/controllers.js
+++ EventManagementSystemApp/www/js/controllers.js
@@ -1,82 +1,101 @@
 angular.module('app.controllers', [])
-  
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('organizerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('venueCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-      
-.controller('personCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('detailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('locationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('eventsByOrganizerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('eventsByVenueCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('registeredPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
- 
+  .controller('homeCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams, $http, ) {
+      $scope.fetchData = function (isReload) {
+        $http.get("http://localhost:1337/")
+          .then(function (response) {
+            $scope.items = response.data;
+            console.log(response.data);
+          }).finally(function () {
+            if (isReload) {
+              console.log('Home page has refreshed!')
+              $scope.$broadcast('scroll.refreshComplete');
+            }
+          });
+      }
+      $scope.fetchData();
+    }
+  ])
+  .controller('organizerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
+  .controller('venueCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
+  .controller('personCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
+  .controller('detailsCtrl', ['$scope', '$stateParams', '$http', '$ionicPopup', '$ionicHistory', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams, $http, $ionicPopup, $ionicHistory) {
+      $http.get("http://localhost:1337/search", {
+          params: {
+            id: $stateParams.id
+          }
+        })
+        .then(function (response) {
+            if (response.data) {
+              $scope.item = response.data;
+              console.log(response.data);
+            } else {
+              var alertPopup = $ionicPopup.alert({
+                title: 'Load Event Failed',
+                template: 'Specified Event has maybe deleted or modified! Please Go back to previous page and refresh.'
+              });
+              alertPopup.then(function (res) {
+                $ionicHistory.goBack();
+              });
+            }
+          },
+          function (response) {
+            var alertPopup = $ionicPopup.alert({
+              title: response.data,
+              template: 'Load Data failed. Please try again.'
+            });
+          }
+        ).finally(function () {
+        });
+    }
+  ])
+  .controller('locationCtrl', ['$scope', '$stateParams', 'venueService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams, venueService) {
+      var venueModel = venueService.getVenue($stateParams.venueId);
+      var map = L.map('map').setView([venueModel.Latitude, venueModel.Longitude], 17);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      L.marker([venueModel.Latitude, venueModel.Longitude]).addTo(map)
+        .bindPopup(venueModel.VenueName);
+    }
+  ])
+  .controller('eventsByOrganizerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
+  .controller('eventsByVenueCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
+  .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
+  .controller('registeredPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {}
+  ])
--- EventManagementSystemApp/www/js/routes.js
+++ EventManagementSystemApp/www/js/routes.js
@@ -1,175 +1,175 @@
 angular.module('app.routes', ['ionicUIRouter'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
 
       .state('tabsController.home', {
-    url: '/page2',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/home.html',
-        controller: 'homeCtrl'
-      }
-    }
-  })
+        url: '/page2',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/home.html',
+            controller: 'homeCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.organizer', {
-    url: '/page3',
-    views: {
-      'tab2': {
-        templateUrl: 'templates/organizer.html',
-        controller: 'organizerCtrl'
-      }
-    }
-  })
+      .state('tabsController.organizer', {
+        url: '/page3',
+        views: {
+          'tab2': {
+            templateUrl: 'templates/organizer.html',
+            controller: 'organizerCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.venue', {
-    url: '/page4',
-    views: {
-      'tab3': {
-        templateUrl: 'templates/venue.html',
-        controller: 'venueCtrl'
-      }
-    }
-  })
+      .state('tabsController.venue', {
+        url: '/page4',
+        views: {
+          'tab3': {
+            templateUrl: 'templates/venue.html',
+            controller: 'venueCtrl'
+          }
+        }
+      })
 
-  .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'templates/tabsController.html',
-    abstract:true
-  })
+      .state('tabsController', {
+        url: '/page1',
+        templateUrl: 'templates/tabsController.html',
+        abstract: true
+      })
 
-  .state('tabsController.person', {
-    url: '/page5',
-    views: {
-      'tab4': {
-        templateUrl: 'templates/person.html',
-        controller: 'personCtrl'
-      }
-    }
-  })
+      .state('tabsController.person', {
+        url: '/page5',
+        views: {
+          'tab4': {
+            templateUrl: 'templates/person.html',
+            controller: 'personCtrl'
+          }
+        }
+      })
 
-  /* 
-    The IonicUIRouter.js UI-Router Modification is being used for this route.
-    To navigate to this route, do NOT use a URL. Instead use one of the following:
-      1) Using the ui-sref HTML attribute:
-        ui-sref='tabsController.details'
-      2) Using $state.go programatically:
-        $state.go('tabsController.details');
-    This allows your app to figure out which Tab to open this page in on the fly.
-    If you're setting a Tabs default page or modifying the .otherwise for your app and
-    must use a URL, use one of the following:
-      /page1/tab1/page6
-      /page1/tab2/page6
-      /page1/tab3/page6
-      /page1/tab4/page6
-  */
-  .state('tabsController.details', {
-    url: '/page6',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/details.html',
-        controller: 'detailsCtrl'
-      },
-      'tab2': {
-        templateUrl: 'templates/details.html',
-        controller: 'detailsCtrl'
-      },
-      'tab3': {
-        templateUrl: 'templates/details.html',
-        controller: 'detailsCtrl'
-      },
-      'tab4': {
-        templateUrl: 'templates/details.html',
-        controller: 'detailsCtrl'
-      }
-    }
-  })
+      /* 
+        The IonicUIRouter.js UI-Router Modification is being used for this route.
+        To navigate to this route, do NOT use a URL. Instead use one of the following:
+          1) Using the ui-sref HTML attribute:
+            ui-sref='tabsController.details'
+          2) Using $state.go programatically:
+            $state.go('tabsController.details');
+        This allows your app to figure out which Tab to open this page in on the fly.
+        If you're setting a Tabs default page or modifying the .otherwise for your app and
+        must use a URL, use one of the following:
+          /page1/tab1/page6
+          /page1/tab2/page6
+          /page1/tab3/page6
+          /page1/tab4/page6
+      */
+      .state('tabsController.details', {
+        url: '/page6/:id',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/details.html',
+            controller: 'detailsCtrl'
+          },
+          'tab2': {
+            templateUrl: 'templates/details.html',
+            controller: 'detailsCtrl'
+          },
+          'tab3': {
+            templateUrl: 'templates/details.html',
+            controller: 'detailsCtrl'
+          },
+          'tab4': {
+            templateUrl: 'templates/details.html',
+            controller: 'detailsCtrl'
+          }
+        }
+      })
 
-  /* 
-    The IonicUIRouter.js UI-Router Modification is being used for this route.
-    To navigate to this route, do NOT use a URL. Instead use one of the following:
-      1) Using the ui-sref HTML attribute:
-        ui-sref='tabsController.location'
-      2) Using $state.go programatically:
-        $state.go('tabsController.location');
-    This allows your app to figure out which Tab to open this page in on the fly.
-    If you're setting a Tabs default page or modifying the .otherwise for your app and
-    must use a URL, use one of the following:
-      /page1/tab1/page7
-      /page1/tab2/page7
-      /page1/tab3/page7
-      /page1/tab4/page7
-  */
-  .state('tabsController.location', {
-    url: '/page7',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/location.html',
-        controller: 'locationCtrl'
-      },
-      'tab2': {
-        templateUrl: 'templates/location.html',
-        controller: 'locationCtrl'
-      },
-      'tab3': {
-        templateUrl: 'templates/location.html',
-        controller: 'locationCtrl'
-      },
-      'tab4': {
-        templateUrl: 'templates/location.html',
-        controller: 'locationCtrl'
-      }
-    }
-  })
+      /* 
+        The IonicUIRouter.js UI-Router Modification is being used for this route.
+        To navigate to this route, do NOT use a URL. Instead use one of the following:
+          1) Using the ui-sref HTML attribute:
+            ui-sref='tabsController.location'
+          2) Using $state.go programatically:
+            $state.go('tabsController.location');
+        This allows your app to figure out which Tab to open this page in on the fly.
+        If you're setting a Tabs default page or modifying the .otherwise for your app and
+        must use a URL, use one of the following:
+          /page1/tab1/page7
+          /page1/tab2/page7
+          /page1/tab3/page7
+          /page1/tab4/page7
+      */
+      .state('tabsController.location', {
+        url: '/page7/:venueId',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/location.html',
+            controller: 'locationCtrl'
+          },
+          'tab2': {
+            templateUrl: 'templates/location.html',
+            controller: 'locationCtrl'
+          },
+          'tab3': {
+            templateUrl: 'templates/location.html',
+            controller: 'locationCtrl'
+          },
+          'tab4': {
+            templateUrl: 'templates/location.html',
+            controller: 'locationCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.eventsByOrganizer', {
-    url: '/page8',
-    views: {
-      'tab2': {
-        templateUrl: 'templates/eventsByOrganizer.html',
-        controller: 'eventsByOrganizerCtrl'
-      }
-    }
-  })
+      .state('tabsController.eventsByOrganizer', {
+        url: '/page8',
+        views: {
+          'tab2': {
+            templateUrl: 'templates/eventsByOrganizer.html',
+            controller: 'eventsByOrganizerCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.eventsByVenue', {
-    url: '/page9',
-    views: {
-      'tab3': {
-        templateUrl: 'templates/eventsByVenue.html',
-        controller: 'eventsByVenueCtrl'
-      }
-    }
-  })
+      .state('tabsController.eventsByVenue', {
+        url: '/page9',
+        views: {
+          'tab3': {
+            templateUrl: 'templates/eventsByVenue.html',
+            controller: 'eventsByVenueCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.login', {
-    url: '/page11',
-    views: {
-      'tab4': {
-        templateUrl: 'templates/login.html',
-        controller: 'loginCtrl'
-      }
-    }
-  })
+      .state('tabsController.login', {
+        url: '/page11',
+        views: {
+          'tab4': {
+            templateUrl: 'templates/login.html',
+            controller: 'loginCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.registeredPage', {
-    url: '/page12',
-    views: {
-      'tab4': {
-        templateUrl: 'templates/registeredPage.html',
-        controller: 'registeredPageCtrl'
-      }
-    }
-  })
+      .state('tabsController.registeredPage', {
+        url: '/page12',
+        views: {
+          'tab4': {
+            templateUrl: 'templates/registeredPage.html',
+            controller: 'registeredPageCtrl'
+          }
+        }
+      })
 
-$urlRouterProvider.otherwise('/page1/page2')
+    $urlRouterProvider.otherwise('/page1/page2')
 
 
-});
+  });
